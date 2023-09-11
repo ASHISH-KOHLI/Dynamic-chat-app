4b30f46240e54f109a6e20f71c4ea58eb1b52438
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-09-11 17:59:30 +0530

    show users o users dashboard and create chat form

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/dynamic-chat-app');
  
 const app = require('express')();
-
+const userRoute = require('./routes/userRoute');
 const http = require('http').Server(app);
 
-const userRoute = require('./routes/userRoute');
 
 app.use('/',userRoute);  
+
 
 http.listen(8000, function(){
     console.log('server is running');
