7e40e00ef67807bda708755b10fc1558b2118b17
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-09-12 15:23:59 +0530

    update the user online ofline Status

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 require('dotenv').config();
-
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/dynamic-chat-app');
- 
+
 const app = require('express')();
 const userRoute = require('./routes/userRoute');
+const User = require('./models/userModel')
 const http = require('http').Server(app);
 
 
 app.use('/',userRoute);  
 
+const io = require('socket.io')(http);
+
+const usp = io.of('/user-namespace');
+
+usp.on('connection', async function(socket){
+    console.log('User Connected');
+    
+    var userId = socket.handshake.auth.token;
+   await User.findByIdAndUpdate({_id:userId},{$set:{is_online:'1'}}); 
+
+    socket.on('disconnect', async function(socket){
+        console.log('user Disconnected')
+    var userId = socket.handshake.auth.token;
+
+   await User.findByIdAndUpdate({_id:userId},{$set:{is_online:'0'}}); 
+
+    })
+
+});
 
 http.listen(8000, function(){
     console.log('server is running');
